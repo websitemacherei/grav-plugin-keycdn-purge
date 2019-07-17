a296a08ca5909229a5a1f418a5439e8769e4c7d2
jan <goo@bar.com>
2019-07-17 11:07:40 +0200

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Grav KeyCDN Plugin
+
+This Grav plugin purges a [KeyCDN](https://www.keycdn.com) pull zone when a change in the admin panel happens.
+
+## Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `keycdn-purge`. 
+
+You should now have all the plugin files under `/your/site/grav/user/plugins/keycdn-purge`
+
+## Configuration
+
+Go to the admin panel under Plugins -> Keycdn purge and enter your keycdn api token and the pull zone you want to purge. 
+
+## License
+
+MIT
---
+++ keycdn-purge.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use Grav\Common\Page\Page;
+use Grav\Common\Page\Pages;
+use GuzzleHttp\Client as Guzzle;
+
+const TMP_FILE = '/tmp/grav-keycdn-purge';
+
+class KeycdnPurgePlugin extends Plugin {
+
+  public static function getSubscribedEvents() {
+    return [
+      'onPluginsInitialized' => ['onPluginsInitialized', 0],
+    ];
+  }
+
+  public function onPluginsInitialized() {
+
+    require __DIR__ . '/vendor/autoload.php';
+    
+    if(!\file_exists(TMP_FILE)) {
+      \file_put_contents(TMP_FILE, json_encode([ 'purge' => false, 'purging' => false ]));
+    }
+    
+    if (!$this->isAdmin()) return;
+    
+    $this->enable([
+      'onAdminAfterSave'     => ['purgeCache', 10],
+      'onAdminAfterSaveAs'   => ['purgeCache', 10],
+      'onAdminAfterDelete'   => ['purgeCache', 10],
+      'onAdminAfterAddMedia' => ['purgeCache', 10],
+      'onAdminAfterDelMedia' => ['purgeCache', 10],
+      'onShutdown'           => ['onShutdown', 10000]
+    ]);
+  }
+
+  public function purgeCache() {
+    \file_put_contents(TMP_FILE, json_encode([ 'purge' => true, 'purging' => false ]));
+  }
+
+  public function onShutdown() {
+
+    $client = new Guzzle();
+
+    $token = $this->config->get('plugins.keycdn-purge.token');
+    $zoneId = $this->config->get('plugins.keycdn-purge.zone_id');
+
+    $purge = json_decode(file_get_contents(TMP_FILE));
+
+    if ($purge->purge && !$purge->purging) {
+
+      \file_put_contents(TMP_FILE, json_encode([ 'purge' => false, 'purging' => true ]));
+  
+      $res = $client->get("https://api.keycdn.com/zones/purge/$zoneId.json", [ 'auth' => [ $token, '' ] ]);    
+  
+      $this->grav['log']->info('keycdn-purge: Http Respone Body: ' . $res->getBody());
+      
+      \file_put_contents(TMP_FILE, json_encode([ 'purge' => false, 'purging' => false ]));
+    }
+    
+  }
+}
